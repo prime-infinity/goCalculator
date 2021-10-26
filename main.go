28aa1d3a747cf41c1cc5f344213efb29446b522b
prime-infinity <soe.a.prime@gmail.com>
2021-10-26 23:11:02 +0100

    testing buffio

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func readString(message string, r *bufio.Reader) string /*, error*/ {
+	fmt.Print(message)
+	return message
+}
 
 func main() {
-	fmt.Println("this is a go calculator")
+	reader := bufio.NewReader(os.Stdin)
+	number /*, _*/ := readString("enter a number", reader)
+	fmt.Println(number)
 }
