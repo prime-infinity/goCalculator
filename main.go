530196c58720b51d994c3877f2215ac07d2123e6
prime-infinity <soe.a.prime@gmail.com>
2021-10-26 23:21:53 +0100

    getting user input with bufio

--- main.go
+++ main.go
@@ -4,15 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
-func readString(message string, r *bufio.Reader) string /*, error*/ {
+func readString(message string, r *bufio.Reader) (string, error) {
 	fmt.Print(message)
-	return message
+	number, err := r.ReadString('\n')
+	return strings.TrimSpace(number), err
 }
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	number /*, _*/ := readString("enter a number", reader)
+	number, _ := readString("enter a number", reader)
 	fmt.Println(number)
 }
