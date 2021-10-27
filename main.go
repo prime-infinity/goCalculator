22951d53a13df3e52da471f12b7059893c6c7052
prime-infinity <soe.a.prime@gmail.com>
2021-10-27 11:07:24 +0100

    added operations

--- main.go
+++ main.go
@@ -8,9 +8,34 @@ import (
 	"strings"
 )
 
+func enterOperation() {
+
+	reader := bufio.NewReader(os.Stdin)
+	o, err := readInput("enter operation ( +, - , / , * ): ", reader)
+
+	if err != nil {
+		fmt.Println("there was a fatal problem")
+		os.Exit(3)
+	}
+
+	switch o {
+	case "+":
+		fmt.Println("you chose addition")
+	case "-":
+		fmt.Println("you chose subtraction")
+	case "*":
+		fmt.Println("you chose multiplication")
+	case "/":
+		fmt.Println("you chose division")
+	default:
+		fmt.Println("your operation was not valid")
+		enterOperation()
+	}
+}
+
 func prompt() {
 	reader := bufio.NewReader(os.Stdin)
-	n, err := readString("enter fitst number: ", reader)
+	n, err := readInput("enter fitst number: ", reader)
 
 	if err != nil {
 		fmt.Println("there was a fatal problem")
@@ -24,14 +49,15 @@ func prompt() {
 		prompt()
 	} else {
 		fmt.Println(nn)
+		enterOperation()
 	}
 
 }
 
-func readString(message string, r *bufio.Reader) (string, error) {
+func readInput(message string, r *bufio.Reader) (string, error) {
 	fmt.Print(message)
-	number, err := r.ReadString('\n')
-	return strings.TrimSpace(number), err
+	toRetrun, err := r.ReadString('\n')
+	return strings.TrimSpace(toRetrun), err
 }
 
 func main() {
